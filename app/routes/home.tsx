56ead5679e43224f38d6622f3c1c97a4d748c8b2
Anubhav Biswas <anubhavbiswas25@gmail.com>
2026-02-19 02:17:14 +0530

    hosted images in the cloud storage of puter and also uploaded then and rendered them in the visualizer

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,6 +4,8 @@ import { ArrowRight, ArrowUpIcon, ArrowUpRight, Clock, Layers } from "lucide-rea
 import Button from "components/ui/Button";
 import Upload from "components/Upload";
 import { useNavigate } from "react-router";
+import { useState } from "react";
+import { createProject } from "lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,10 +16,39 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   const navigate = useNavigate()
+  const [projects,setProjects] = useState<DesignItem[]>([])
   const handleUploadComplete = async(bases64Data:string) =>{
     // on completion of the upload we grab the date make the uuid for the image and pass it down to the visualizer route
     const newId = Date.now().toString()
-    navigate(`/visualizer/${newId}`)
+    const name = `Residence ${newId}`
+
+    // new item that will be passed down to the visualizer page
+    const newItem =  {
+      id:newId,
+      name,
+      sourceImage: bases64Data,
+      renderedImage: undefined,
+      timestamp: Date.now()
+    }
+    const saved = await createProject({
+      item: newItem,
+      visibility: 'private'
+    })
+    if(!saved) {
+      console.error('Failed to create project'); 
+      return false;
+    }
+
+    setProjects((prev=>[newItem,...prev]))
+
+    navigate(`/visualizer/${newId}`,{
+      // passing down these state values to the visualizer page
+      state:{
+        initialImage: saved.sourceImage,
+        initialRendered: saved.renderedImage || null,
+        name
+      }
+    })
     return true
   }
   return <div className="home">
@@ -73,29 +104,41 @@ export default function Home() {
           </div>
         </div>
         <div className="projects-grid">
-          <div className="project-card group">
-            <div className="preview">
-              <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="Project" />
-              <div className="badge">
-                <span>Community</span>
-              </div>
-            </div>
-            <div className="card-body">
-              <div>
-                <h3>Project Manhattan</h3>
-                <div className="meta">
-                  <Clock size={12}/>
-                  <span>{new Date('01.01.2026').toLocaleDateString()}</span>
-                  <span>By John Doe</span>
+          {projects.map(({id,name,renderedImage,sourceImage,timestamp})=>(
+            <div className="project-card group">
+              <div className="preview">
+                <img src={renderedImage || sourceImage} 
+                alt="Project" 
+                />
+                <div className="badge">
+                  <span>Community</span>
                 </div>
               </div>
-              <div className="arrow">
-                <ArrowUpRight size={18}/>
+              <div className="card-body">
+                <div>
+                  <h3>{name}</h3>
+                  <div className="meta">
+                    <Clock size={12}/>
+                    <span>{new Date(timestamp).toLocaleDateString()}</span>
+                    <span>By John Doe</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUpRight size={18}/>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
   </div>;
 }
+
+/*
+Puter handles everything in the client-side and there is no involvement of the sever-side handling
+All the server-side processes is handled by the Puter itself
+1. Key-Value : to store data of the user in place of entire db setups
+2. FileSystem+Hosting : to store images in form of files and giving urls of those images via hosting just like we setup buckets in supabase for files
+3.Workers : When handling on the client-side there might be security breaches so, the puter have workers to ensure auth/security between client and server 
+ */
--- lib/puter.action.ts
+++ lib/puter.action.ts
@@ -1,4 +1,6 @@
 import puter from "@heyputer/puter.js";
+import { getOrCreateHostingConfig, uploadImageToHosting } from "./puter.hosting";
+import { isHostedUrl } from "./utils";
 
 // to sign in the user
 export const signIn = async()=> await puter.auth.signIn()
@@ -11,6 +13,69 @@ export const getCurrentUser = async() => {
     try {
         return await puter.auth.getUser()
     } catch (error) {
+        return null;
+    }
+}
+
+// to create projects that are shown in the projects section of the hero
+// here we store the projects data to the puter KV db
+export const createProject = async({item}:CreateProjectParams): Promise<DesignItem | null | undefined> =>{
+    const projectId = item.id;
+
+    const hosting = await getOrCreateHostingConfig()//get the hosting subdomain
+    // this is the source image which we uploaded
+    const hostedSource = projectId ? 
+        await uploadImageToHosting({
+            hosting,
+            url: item.sourceImage,
+            projectId,
+            label: 'source'
+        }) : null
+    
+    // this is the images we will get from the AI corresponding to the uploaded images
+    const hostedRender = projectId && item.renderedImage ? 
+        await uploadImageToHosting({
+            hosting,
+            url: item.renderedImage,
+            projectId,
+            label: 'rendered'
+        }) : null
+    
+        // if hostedSource the url then return it or check if the image is hosted then return the image's url otherwise empty string
+    const resolvedSource = hostedSource?.url || (isHostedUrl(item.sourceImage) ? 
+        item.sourceImage : ''
+    )
+
+    if(!resolvedSource){
+        console.warn('Failed to host source image, skipping save.')
+        return null;
+    }
+
+    const resolvedRender = hostedRender?.url ?
+        hostedRender?.url
+        : item.renderedImage && isHostedUrl(item.renderedImage) ?
+            item.renderedImage : undefined;
+    
+    // fetching values from the item 
+    const {
+        sourcePath: _sourcePath,
+        renderedPath: _renderedPath,
+        publicPath: _publicPath,
+        ...rest
+    } = item
+
+    // payload values are passed on and resolved image of both source and render
+    const payload = {
+        ...rest,
+        sourceImage: resolvedSource,
+        renderedImage: resolvedRender
+    }
+    try {
+        // calling the puter worker to store the project in kv db
+
+        return payload
+    } catch (error) {
+        console.log('Failed to save project',error)
         return null;
     }
 }
